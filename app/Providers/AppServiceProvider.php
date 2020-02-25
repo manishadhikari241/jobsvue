890d58aabf1a29615d0cb2e0defc9b22280c58bb
manishadhikari241 <manishadhikari241@gmail.com>
2020-02-25 17:11:11 +0545

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\AboutRepositoryRepository;
 use App\Repositories\Contracts\JobsCategoryRepository;
-<<<<<<< HEAD
 use App\Repositories\Eloquent\EloquentAboutRepositoryRepository;
-=======
 use App\Repositories\Contracts\LoginRegisterRepository;
->>>>>>> master
 use App\Repositories\Eloquent\EloquentJobsCategoryRepository;
 use App\Repositories\Eloquent\EloquentLoginRegisterRepository;
 use Illuminate\Support\Facades\Schema;
@@ -24,11 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(JobsCategoryRepository::class, EloquentJobsCategoryRepository::class);
-<<<<<<< HEAD
         $this->app->singleton(AboutRepositoryRepository::class, EloquentAboutRepositoryRepository::class);
-=======
         $this->app->singleton(LoginRegisterRepository::class, EloquentLoginRegisterRepository::class);
->>>>>>> master
     }
 
     /**
